2f1d9b00946d5a2264dcc89dceef34eba16bf8b5
小倉裕一 <j211025ogu@gmail.com>
2017-11-17 18:29:17 +0900

    add pet interface

--- interface/main.go
+++ interface/main.go
@@ -7,6 +7,12 @@ type Animal interface {
 	Cry()
 }
 
+// Pet pet interface.
+type Pet interface {
+	Choker()
+	LikeHuman()
+}
+
 // Dog struct
 type Dog struct{}
 
@@ -15,12 +21,40 @@ func (d *Dog) Cry() {
 	fmt.Println("わんわん")
 }
 
+// Choker dog choker method.
+func (d *Dog) Choker() {
+	fmt.Println("リード付きの首輪")
+}
+
+// LikeHuman dog like human method.
+func (d *Dog) LikeHuman() {
+	fmt.Println("ご主人様大好き")
+}
+
 // Cat struct
 type Cat struct{}
 
 // Cry cat cry method
 func (c *Cat) Cry() {
 	fmt.Println("にゃーにゃー")
+}
+
+// Choker cat choker method.
+func (c *Cat) Choker() {
+	fmt.Println("鈴付きの首輪")
+}
+
+// LikeHuman cat like human method.
+func (c *Cat) LikeHuman() {
+	fmt.Println("ご主人様よりこたつ")
+}
+
+// Elephant elephant struct
+type Elephant struct{}
+
+// Cry elephant cry method.
+func (e *Elephant) Cry() {
+	fmt.Println("パオーン!")
 }
 
 // Stone struct
@@ -43,15 +77,31 @@ func MakeSomeoneCry(someone interface{}) {
 	a.Cry()
 }
 
+// MakeSomeoneHasChoker choker func
+func MakeSomeoneHasChoker(someone interface{}) {
+	fmt.Println("どんな首輪してる？")
+	p, ok := someone.(Pet)
+	if !ok {
+		fmt.Println("ペットじゃないから首輪してないよ")
+		return
+	}
+	p.Choker()
+}
+
 func main() {
 	dog := new(Dog)
 	cat := new(Cat)
 	stone := new(Stone)
+	elephant := new(Elephant)
 	MakeSomeoneCry(dog)
 	MakeSomeoneCry(cat)
+	MakeSomeoneCry(elephant)
 	MakeSomeoneCry(stone)
 	// MakeAnimalCry(dog)
 	// MakeAnimalCry(cat)
+	MakeSomeoneHasChoker(dog)
+	MakeSomeoneHasChoker(cat)
+	MakeSomeoneHasChoker(elephant)
 
 	// h := Hex(10)
 	// str := h.String()
