ae94d9a61db1ea2c6ff6f03ba9f37c122012dc1f
小倉裕一 <j211025ogu@gmail.com>
2017-11-17 18:08:48 +0900

    add get start of month and end of month

--- time/main.go
+++ time/main.go
@@ -40,6 +40,7 @@ func main() {
 
 	createTimeStruct()
 	timeToString()
+	operationMonth()
 }
 
 func createTimeStruct() {
@@ -54,4 +55,14 @@ func timeToString() {
 	fmt.Println(t.Format(dateFormat), reflect.TypeOf(t.Format(dateFormat))) // 2017-11-15 string
 	// parse to datetime string.
 	fmt.Println(t.Format(timeFormat)) // 2017-11-15 10:44:32
+}
+
+func operationMonth() {
+	t := time.Now()
+	// get start of this month.
+	start := t.AddDate(0, 0, -1*t.Day()+1).Format(dateFormat)
+	fmt.Println(start)
+	// get end of this month.
+	end := t.AddDate(0, 1, -1*t.Day()).Format(dateFormat)
+	fmt.Println(end)
 }
