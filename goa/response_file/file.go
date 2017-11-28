87ecb02c0b35906ee53800c6e6cbbf9c00319a30
小倉裕一 <j211025ogu@gmail.com>
2017-11-28 19:02:18 +0900

    delete unneeded

--- goa/response_file/file.go
+++ goa/response_file/file.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"io/ioutil"
+	"log"
 
 	"github.com/go-examples/goa/response_file/app"
 	"github.com/goadesign/goa"
-	"github.com/k0kubun/pp"
 )
 
 // FileController implements the file resource.
@@ -20,17 +20,13 @@ func NewFileController(service *goa.Service) *FileController {
 
 // File runs the file action.
 func (c *FileController) File(ctx *app.FileFileContext) error {
-	// FileController_File: start_implement
-
-	// Put your logic here
-	f, err := ioutil.ReadFile("./public/sample.xlsx")
+	// get excel file
+	res, err := ioutil.ReadFile("./public/sample.xlsx")
 	if err != nil {
 		return err
 	}
-	pp.Println(f)
+	// set response header for content disposition
 	ctx.ResponseData.Header().Set("Content-Disposition", "attachment; filename=sample.xlsx")
-	res := f
-	// ctx.ResponseData.Write(res)
-	// FileController_File: end_implement
+	log.Println("download excel file")
 	return ctx.OK(res)
 }
